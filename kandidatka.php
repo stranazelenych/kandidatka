39e7585b2b549b60c437335dc85609a4fd155a1e
Filip Zrůst <filip.zrust@sinsir.net>
2014-04-05 18:26:04 +0200

    Custom template support
    The code in `admin` directory is basically a copy of original WordPress code
    modified for our case.

--- kandidatka.php
+++ kandidatka.php
@@ -4,6 +4,8 @@
  * @version 0.1
  */
 
+require_once __DIR__ . '/admin/meta-boxes.php';
+
 /**
  * Plugin Name: Kandidátka
  * Plugin URI: https://github.com/stranazelenych/kandidatka
@@ -34,9 +36,59 @@ function sz_kandidatka_init() {
 			'new_item' => 'Vytvořit kandidátku'
 		),
 		'public' => true,
+		'capability_type' => 'page',
 		'supports' => array(
-			'title', 'editor', 'custom-fields'
+			'title', 'editor', 'thumbnail', 'custom-fields',
+			'page-attributes'
+		),
+		'rewrite' => array(
+			'slug' => 'kandidatka'
 		)
 	));
 }
 add_action( 'init', 'sz_kandidatka_init' );
+
+/**
+ * Saves custom template for "sz_kandidatka" post.
+ *
+ * It uses metadata with "_sz_kandidatka_template" name.
+ *
+ * @param integer $post_id
+ * @param object $post
+ */
+function sz_kandidatka_save_template( $post_id, $post ) {
+	if ( ( 'sz_kandidatka' != $post->post_type ) ||
+	     empty( $_POST['sz_kandidatka_template'] ) ) {
+		return;
+	}
+
+	update_post_meta(
+		$post->ID,
+		'_sz_kandidatka_template',
+		$_POST['sz_kandidatka_template']
+	);
+}
+add_action( 'save_post', 'sz_kandidatka_save_template', 10, 2 );
+
+/**
+ * Loads 
+ */
+function sz_kandidatka_get_template_for_template_loader( $template ) {
+	global $wp_query;
+
+	$post = $wp_query->get_queried_object();
+	if ( $post ) {
+		$post_template = get_post_meta(
+			$post->ID, '_sz_kandidatka_template', true
+		);
+
+		if ( !empty( $post_template ) && ( 'default' != $post_template ) ) {
+			$template = get_stylesheet_directory() . '/' . $post_template;
+		}
+	}
+
+	return $template;
+}
+add_filter(
+	'template_include', 'sz_kandidatka_get_template_for_template_loader', 99
+);
